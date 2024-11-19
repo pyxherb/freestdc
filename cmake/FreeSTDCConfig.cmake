a8b5813d0116f15ed4c249eb4b0e94ca75885078
pyxherb <codesbuilder@163.com>
2024-11-20 06:45:14 +0800

    Initial commit

---
+++ cmake/FreeSTDCConfig.cmake
@@ -0,0 +1,26 @@
+foreach(i ${CMAKE_SYSTEM_PREFIX_PATH})
+	message(CHECK_START "Finding include directory of FreeSTDC: ${i}/freestdc/include")
+
+	find_path(FREESTDC_INCLUDE_DIRS NAMES "freestdc.h" HINTS "${i}/freestdc/include/freestdc")
+
+	if (FREESTDC_INCLUDE_DIRS)
+		message(CHECK_PASS "Found include directory of PbOS Commons: ${FREESTDC_INCLUDE_DIRS}")
+		break()
+	endif()
+endforeach()
+
+foreach(i ${CMAKE_SYSTEM_PREFIX_PATH})
+	message(CHECK_START "Finding library of FreeSTDC: ${i}/freestdc/include")
+
+	find_library(FREESTDC_LINK_LIBRARIES NAMES freestdc HINTS "${i}/freestdc/lib")
+
+	if (FREESTDC_LINK_LIBRARIES)
+		message(CHECK_PASS "Found library of FreeSTDC: ${FREESTDC_LINK_LIBRARIES}")
+		break()
+	endif()
+endforeach()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+    FreeSTDC
+    REQUIRED_VARS FREESTDC_INCLUDE_DIRS FREESTDC_LINK_LIBRARIES)
---
+++ include/freestdc.h
@@ -0,0 +1,6 @@
+#ifndef _FREESTDC_FREESTDC_H_
+#define _FREESTDC_FREESTDC_H_
+
+#define freestdc_VERSION 0.0.1
+
+#endif
